99478ab5a8fa48aa85bdac2a190900ce756e0ad1
raushanraja <vraushanraja@gmail.com>
2024-07-27 02:48:24 +0530

    add indent indication

--- lua/custom/plugins/indent.lua
+++ lua/custom/plugins/indent.lua
@@ -2,8 +2,47 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
+    config = function()
+      local highlight = {
+        'RainbowRed',
+        'RainbowYellow',
+        'RainbowBlue',
+        'RainbowOrange',
+        'RainbowGreen',
+        'RainbowViolet',
+        'RainbowCyan',
+      }
+      local hooks = require 'ibl.hooks'
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+      end)
+
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      require('ibl').setup { scope = { highlight = highlight } }
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
   },
   -- autopairing of (){}[] etc
-  { 'windwp/nvim-autopairs', event = 'InsertEnter', config = true },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local autopairs_opts = {
+        fast_wrap = {},
+        disable_filetype = { 'TelescopePrompt', 'vim' },
+      }
+      require('nvim-autopairs').setup(autopairs_opts)
+      require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
   { 'tpope/vim-surround' },
 }
