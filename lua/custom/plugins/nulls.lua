6743894471c1b855a371cfadb9d41282edaabb0c
raushanraja <vraushanraja@gmail.com>
2024-11-06 01:02:42 +0530

    feat: add tsserver & nullls

---
+++ lua/custom/plugins/nulls.lua
@@ -0,0 +1,23 @@
+return {
+  'nvimtools/none-ls.nvim',
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+    'nvim-lua/plenary.nvim',
+  },
+  opts = function(_, opts)
+    local nls = require 'null-ls'
+    opts.root_dir = opts.root_dir or require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git')
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      nls.builtins.formatting.fish_indent,
+      nls.builtins.diagnostics.fish,
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.shfmt,
+      -- nls.builtins.diagnostics.pylint,
+      -- nls.builtins.diagnostics.mypy,
+      require 'none-ls.diagnostics.ruff',
+      require('none-ls.diagnostics.eslint').with {
+        args = { '--format', 'json' },
+      },
+    })
+  end,
+}
