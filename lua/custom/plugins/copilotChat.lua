0b7f7c6139aa36249f92978c021b2d8632dd486b
raushanraja <vraushanraja@gmail.com>
2024-12-09 03:39:57 +0530

    comment plugin

--- lua/custom/plugins/copilotChat.lua
+++ lua/custom/plugins/copilotChat.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
-    dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = false, -- Enable debugging
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+  --   {
+  --     'CopilotC-Nvim/CopilotChat.nvim',
+  --     branch = 'canary',
+  --     dependencies = {
+  --       { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+  --       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+  --     },
+  --     opts = {
+  --       debug = false, -- Enable debugging
+  --     },
+  --     -- See Commands section for default commands if you want to lazy load on them
+  --   },
 }
